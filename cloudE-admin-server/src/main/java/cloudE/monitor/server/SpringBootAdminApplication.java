846ac838c7436c764de33c8171dc1101c37756ed
carl.gao <carl.gao@circle.us>
2017-07-24 17:27:48 +0800

    增加spring boot admin

---
+++ cloudE-admin-server/src/main/java/cloudE/monitor/server/SpringBootAdminApplication.java
@@ -0,0 +1,21 @@
+
+package cloudE.monitor.server;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author vangao1989
+ * @Description: spring cloud admin
+ * @date 2017年7月24日
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableAdminServer
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
